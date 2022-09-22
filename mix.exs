81aa072076805286a6ce72592fca7ce6bb85017b
Sgiath <sgiath@sgiath.dev>
2022-09-22 10:53:40 +0200

    docs: initialize
    Signed-off-by: Sgiath <sgiath@sgiath.dev>

--- mix.exs
+++ mix.exs
@@ -6,12 +6,20 @@ defmodule Secp256k1.MixProject do
       app: :secp256k1,
       version: "0.1.0",
       elixir: "~> 1.14",
-      start_permanent: Mix.env() == :prod,
       deps: deps(),
 
       # Elixir make
       compilers: [:elixir_make] ++ Mix.compilers(),
-      make_clean: ["clean"]
+      make_clean: ["clean"],
+
+      # Docs
+      name: "secp256k1",
+      source_url: "https://git.sr.ht/~sgiath/secp256k1",
+      homepage_url: "https://git.sr.ht/~sgiath/secp256k1",
+      description: """
+      Library wrapping around secp256k1 Bitcoin library
+      """,
+      docs: docs()
     ]
   end
 
@@ -23,7 +31,42 @@ defmodule Secp256k1.MixProject do
 
   defp deps do
     [
-      {:elixir_make, "~> 0.4", runtime: false}
+      {:elixir_make, "~> 0.6", runtime: false},
+      {:ex_doc, "~> 0.28", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:mix_test_watch, "~> 1.1", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      authors: [
+        "Sgiath <sgiath@sgiath.dev>"
+      ],
+      main: "overview",
+      formatters: ["html"],
+      source_url_patter: "https://git.sr.ht/~sgiath/secp256k1/tree/master/item/%{path}#L%{line}",
+      extra_section: "Guides",
+      extras: extras(),
+      groups_for_extras: groups_for_extras(),
+      nest_modules_by_prefix: [
+        Spaceboy.Middleware
+      ]
+    ]
+  end
+
+  defp extras do
+    [
+      # Introduction
+      "docs/introduction/overview.md",
+      "docs/introduction/installation.md"
+    ]
+  end
+
+  defp groups_for_extras do
+    [
+      Introduction: ~r/docs\/introduction\/.?/,
+      Guides: ~r/docs\/guides\/.?/
     ]
   end
 end
